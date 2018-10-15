0ad94903787fe2a54fe604520674d94784af3934
aatack <alexanderjtatack@gmail.com>
2018-10-15 12:47:54 +0100

    Implement Tuple2 for use in Unity

---
+++ Bubbles/Util/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace Bubbles.Util
+{
+	public class Pair<T1, T2>
+	{
+		public readonly T1 First;
+		public readonly T2 Second;
+
+		public Pair(T1 first, T2 second)
+		{
+			First = first;
+			Second = second;
+		}
+	}
+}
