aac4ff78a90c38c143541584f09c74dc7fc0754d
aatack <alexanderjtatack@gmail.com>
2018-10-25 17:50:33 +0100

    Define behaviour of the position data type

---
+++ Bubbles/Templates/IPosition.cs
@@ -0,0 +1,22 @@
+﻿namespace Bubbles.Templates
+{
+	/// <summary>
+	/// Defines a data type which can be considered
+	/// to be some kind of position, spatially, temporally,
+	/// or otherwise, due to its implementation of a
+	/// similarity metric.
+	/// </summary>
+	public interface IPosition<T> where T : IPosition<T>
+	{
+
+		/// <summary>
+		/// Calculate the similarity of one point to a different point
+		/// using the type's similarity metric.  A lower value means
+		/// more similar points, where a similarity of 0 implies that
+		/// the points are identical.
+		/// </summary>
+		/// <param name="other">Other.</param>
+		float SimilarityTo(T other);
+
+	}
+}
