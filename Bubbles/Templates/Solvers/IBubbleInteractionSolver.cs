1a0bdbf387aeb2351add9901404359e8d9a21464
aatack <alexanderjtatack@gmail.com>
2018-11-01 00:01:12 +0000

    Implement interface defining bubble interaction solver

---
+++ Bubbles/Templates/Solvers/IBubbleInteractionSolver.cs
@@ -0,0 +1,29 @@
+﻿using Bubbles.Templates;
+using System;
+
+namespace Bubbles.Templates.Solvers
+{
+	/// <summary>
+	/// Defines the behaviours required of a class which is capable of resolving
+	/// discontinuities between pairs of bubbles.
+	/// </summary>
+	public interface IBubbleInteractionSolver<Position>
+		where Position : IPosition<Position>
+	{
+
+		void AddInteraction<TypeA, TypeB>(
+			Func<DiscontinuousBubble<Position>, DiscontinuousBubble<Position>, float> nextDiscontinuity,
+			Action<DiscontinuousBubble<Position>, DiscontinuousBubble<Position>> resolveDiscontinuity)
+			where TypeA : DiscontinuousBubble<Position>
+			where TypeB : DiscontinuousBubble<Position>;
+
+		float NextDiscontinuity(
+			DiscontinuousBubble<Position> a,
+			DiscontinuousBubble<Position> b);
+
+		void ResolveNextDiscontinuity(
+			DiscontinuousBubble<Position> a,
+			DiscontinuousBubble<Position> b);
+
+	}
+}
